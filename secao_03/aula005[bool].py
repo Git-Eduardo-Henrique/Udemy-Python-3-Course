e516dfaeee14be5f5611b09434497d77e8c0739e
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-27 22:53:27 -0300

    aula005

---
+++ secao_03/aula005[bool].py
@@ -0,0 +1,9 @@
+print(100 * "=")
+
+#bool = True(verdadeiro) ou False(falso)
+#== : verifica se um valor é igual ao outro
+print("10 é igual a 10? ", 10 == 10)
+print("10 é igual a 11? ", 10 == 11)
+print("True e False são do tipo: ", type(True))
+
+print(100 * "=")
