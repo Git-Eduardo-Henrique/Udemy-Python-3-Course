108c34b899180df8f64b99a3866b6a0522615a81
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-27 22:45:45 -0300

    aula004

---
+++ secao_03/aula004[int_float].py
@@ -0,0 +1,9 @@
+print(100 * "=")
+
+# int = numero inteiro
+# float = numero decimal
+print("isso é um numero inteiro (int): ", 11, -11, 0, type(0))
+print("isso é um numero decimal (float): ", 11.11, -11.11, 0.0, type(0.0))
+#type()= "função" que mostra o tipo do valor
+
+print(100 * "=")
