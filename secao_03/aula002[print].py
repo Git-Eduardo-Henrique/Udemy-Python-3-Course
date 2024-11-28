9b2a5748530988cfacb3a0dbf52cc0ab87ea11f5
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-27 22:26:11 -0300

    aula002

---
+++ secao_03/aula002[print].py
@@ -0,0 +1,4 @@
+#mostra valores na tela
+print(12, 34, sep="-") #sep= define o separador no terminal
+print(56, 78, sep="_", end="\n") #muda oque acontece no final da linha
+# \n = padrão/quebra de linha
