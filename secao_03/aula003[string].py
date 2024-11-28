d33378df30099e572abee3fa4736342edd036f07
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-27 22:33:39 -0300

    aula003

---
+++ secao_03/aula003[string].py
@@ -0,0 +1,6 @@
+# string = valor de texto entre aspas
+print("isso é uma string")
+
+# para mostrar alguma aspas em uma string deve se fazer desta forma:
+print("string com ''")
+print('string com ""')
