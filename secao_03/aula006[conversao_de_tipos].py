bb3b81747ef2ebd5a2dcb68df1ce9b8f73204dda
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-28 22:16:41 -0300

    aula006

---
+++ secao_03/aula006[conversao_de_tipos].py
@@ -0,0 +1,13 @@
+print(100 * "=")
+
+# int() = transforma numero de string em inteiro
+# float() = transforma numero de string em flutuante
+# string() = transforma numero em texto string
+# bool() = transforma algum valor em false ou true (1)(0)
+
+print("'1' convertido para numero inteiro:", int("1"), type(int("1")))
+print("'2.2' convertido para numero flutuante:", float("2.2"), type(float("2.2")))
+print("3 convertido para string:", str(3), type(str(3)))
+print("1 convertido para boleano:", bool(1), type(bool(1)))
+
+print(100 * "=")
