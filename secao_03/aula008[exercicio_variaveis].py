2889f030ce8a2a019c3ac4363a4b0f66ecc6b47f
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-28 22:37:03 -0300

    aula008

---
+++ secao_03/aula008[exercicio_variaveis].py
@@ -0,0 +1,17 @@
+print(100 * "=")
+
+nome = "Eduardo"
+sobrenome = "Henrique"
+idade = 20
+maior_de_idade = idade >= 18
+ano_nasc = 2024 - idade
+altura = 1.85
+
+print("Seu nome:", nome)
+print("Seu sobrenome:", sobrenome)
+print("Sua idade:", idade)
+print("Seu ano de nascimento:", ano_nasc)
+print("É maior de idade?", maior_de_idade)
+print("Sua altura", altura, "m")
+
+print(100 * "=")
