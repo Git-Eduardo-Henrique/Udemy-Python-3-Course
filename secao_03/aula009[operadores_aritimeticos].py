129a59d03f11f4bfcd3b9ed12fc36e33c73e49db
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-28 22:48:40 -0300

    aula009

---
+++ secao_03/aula009[operadores_aritimeticos].py
@@ -0,0 +1,19 @@
+print(100 * "=")
+
+adicao = 2 + 2
+subtracao = 2 - 2
+multiplicacao = 2 * 2
+divisao = 2 / 2
+divisao_inteira = 5 // 2
+potenciacao = 2 ** 2
+resto_divisao = 13 % 3
+
+print("2 + 2:", adicao)
+print("2 - 2:", subtracao)
+print("2 x 2:", multiplicacao)
+print("2 / 2:", divisao)
+print("5 / 2 (divisão inteira):", divisao_inteira)
+print("2²:", potenciacao)
+print("13 / 3 (resto da divisão):", resto_divisao)
+
+print(100 * "=")
