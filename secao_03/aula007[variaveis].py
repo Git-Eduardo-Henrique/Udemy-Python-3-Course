d1d16cba54168777d9adf37ee13578852a74805e
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-28 22:29:00 -0300

    aula007

---
+++ secao_03/aula007[variaveis].py
@@ -0,0 +1,12 @@
+print(100 * "=")
+
+#variaveis guardam informações na memoria do computador
+nome = "Eduardo Henrique"
+idade = 20
+# >= verifica se é maior ou igual a algo
+maior_de_idade = idade >= 18
+
+print("Seu nome:", nome, "\nSua idade:", idade)
+print("É maior de idade?", maior_de_idade)
+
+print(100 * "=")
