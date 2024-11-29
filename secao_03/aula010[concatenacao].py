f3a6e1fe971c16ffd554cfdeb2f3615c0e3e449d
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-28 23:02:17 -0300

    aula010

---
+++ secao_03/aula010[concatenacao].py
@@ -0,0 +1,11 @@
+print(100 * "=")
+
+# junto todas as strings
+texto_somado = "Eduardo" + " " + "Henrique"
+# repete a string um numero de vezes
+a_50_vezes = "a" * 50
+
+print(texto_somado)
+print(a_50_vezes)
+
+print(100 * "=")
