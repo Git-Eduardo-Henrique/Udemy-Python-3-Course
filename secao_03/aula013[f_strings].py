82221f5881409656b8815fb80e4845224180f9db
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-29 22:24:20 -0300

    aula013

---
+++ secao_03/aula013[f_strings].py
@@ -0,0 +1,12 @@
+print(100 * "=")
+
+nome = "Eduardo Henrique"
+altura = 1.85
+peso = 90
+imc = peso / (altura * altura)
+
+# f"" = f-strings
+print(f"seu nome é {nome}, você tem {altura:.2f} m de altura e {peso}kg de peso")
+print(f"seu imc é de: {imc:.2f}")
+
+print(100 * "=")
