bec8cb5bbc74a12fd066b24ce2556b20deb353f6
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-29 22:18:31 -0300

    aula012

---
+++ secao_03/aula012[exercicio_imc].py
@@ -0,0 +1,11 @@
+print(100 * "=")
+
+nome = "Eduardo Henrique"
+altura = 1.85
+peso = 90
+imc = peso / (altura * altura)
+
+print("seu nome é:", nome, "você tem", altura, "m de altura e", peso, "kg de peso")
+print("seu imc é de:", imc)
+
+print(100 * "=")
