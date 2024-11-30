52c10a8ced8595dede4136af3ace8aa51116907c
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-29 22:52:15 -0300

    aula016

---
+++ secao_03/aula016[if_elif_else].py
@@ -0,0 +1,13 @@
+print(100 * "=")
+
+entrada = input("Você deseja entrar ou sair? ")
+
+print(100 * "=")
+if entrada == "entrar":
+    print("você entrou no sistema")
+elif entrada == "sair":
+    print("você saiu do sistema")
+else:
+    print("valor inválido!!!")
+    
+print(100 * "=")
