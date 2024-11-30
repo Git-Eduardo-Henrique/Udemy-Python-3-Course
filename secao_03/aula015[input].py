ec79019de48e52616cfe6192d2e0e4187e019b99
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-29 22:42:51 -0300

    aula015

---
+++ secao_03/aula015[input].py
@@ -0,0 +1,11 @@
+print(100 * "=")
+
+# coleta um dado no terminal
+numero_1 = int(input("Digite o primeiro numero: "))
+numero_2 = int(input("Digite o segundo numero: "))
+conta = numero_1 + numero_2
+
+print(100 * "=")
+print(f"{numero_1} + {numero_2} = {conta}")
+
+print(100 * "=")
