35fc9175f0e678acd87b3d65aa8ad68131b4de22
Eduardo Henrique <eduardos8236@gmail.com>
2024-11-29 22:32:40 -0300

    aula014

---
+++ secao_03/aula014[format].py
@@ -0,0 +1,12 @@
+print(100 * "=")
+
+nome = "Eduardo Henrique"
+altura = 1.85
+peso = 90
+imc = peso / (altura * altura)
+
+# format = f strings datada
+print("seu nome é {}, você tem {:.2f} m de altura e {}kg de peso".format(nome, altura, peso))
+print("seu imc é de: {:.2f}".format(imc))
+
+print(100 * "=")
