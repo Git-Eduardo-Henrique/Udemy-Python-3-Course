9b94927fcd2fcf990b860057362b1b47be94008b
Eduardo Henrique <eduardos8236@gmail.com>
2024-12-18 14:18:11 -0300

    aula019

---
+++ secao_03/aula019[operadores_de_comparacao].py
@@ -0,0 +1,17 @@
+maior = 2 > 2
+maior_igual = 2 >= 2
+menor = 2 < 1
+menor_igual = 2 <= 2
+igual = 2 == 3
+diferente = 2 != 2
+
+print(100 * "=")
+
+print(f"2 é maior que 2? {maior}")
+print(f"2 é maior ou igual a 2? {maior_igual}")
+print(f"2 é menor que 1? {menor}")
+print(f"2 é menor ou igual a 2? {menor_igual}")
+print(f"2 é igual a 3? {igual}")
+print(f"2 é diferente de 2? {diferente}")
+
+print(100 * "=")
