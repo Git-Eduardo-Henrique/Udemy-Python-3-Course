008ae501d8855342ac5a534a9ca386238f5cca3b
Eduardo Henrique <eduardos8236@gmail.com>
2024-12-20 22:33:30 -0300

    aula027

---
+++ secao_03/aula027[len].py
@@ -0,0 +1,11 @@
+print(100 * "=")
+
+nome = input("digite seu nome: ")
+
+# len() diz o tamanho da variavel
+# [i:f:p] = i(inicio), f(fim), p(passo)
+
+print(f"{nome} tem {len(nome)} caracteres")
+print(f"seu nome de traz para frente: {nome[::-1]}")
+
+print(100 * "=")
