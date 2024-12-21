2dec9a8a70be293a60be5d483dd717c8ca0ad7e1
Eduardo Henrique <eduardos8236@gmail.com>
2024-12-20 22:12:11 -0300

    aula025

---
+++ secao_03/aula025[interpolacao].py
@@ -0,0 +1,10 @@
+print(100 * "=")
+
+nome = input("digite seu nome: ")
+dindin = int(input("quanto dinheiro voce tem: "))
+
+print(100 * "=")
+print("%s tem R$ %i" % (nome, dindin))
+print("Hexadecimal de %i: %04x" % (dindin, dindin))
+
+print(100 * "=")
