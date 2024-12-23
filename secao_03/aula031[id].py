83465a6614906c265d09e19fc15c562ef39d702e
Eduardo Henrique <eduardos8236@gmail.com>
2024-12-23 14:38:22 -0300

    aula031

---
+++ secao_03/aula031[id].py
@@ -0,0 +1,10 @@
+print(100 * "=")
+
+digitado = input("digite algo: ")
+
+print(100 * "=")
+
+# id() = mostra local da variavel na memoria
+print(f"local de '{digitado}' na memoria: {id(digitado)}")
+
+print(100 * "=")
