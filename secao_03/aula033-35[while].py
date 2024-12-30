e70117628ee6cd031998c40d65ed1a559a811d70
Eduardo Henrique <eduardos8236@gmail.com>
2024-12-30 14:39:03 -0300

    aula035

---
+++ secao_03/aula033-35[while].py
@@ -0,0 +1,12 @@
+contador = 0
+
+print(100 * "=")
+print("contador de 1 até 10")
+print(100 * "=")
+
+while contador < 10:
+    contador = contador + 1
+    print(contador, end=" -> ")
+
+print("ACABOU!")
+print(100 * "=")
