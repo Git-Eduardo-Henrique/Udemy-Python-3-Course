fbf10bb16d1639f754e2a74fb7adb63d0760de4e
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-08 23:04:09 -0300

    aula056

--- secao_03/aula056[split_strip_join].py
+++ secao_03/aula056[split_strip_join].py
@@ -3,11 +3,13 @@ lista_palavras = frase.split() # sepera por espaços caso não explicificar
 lista_frases = frase.split(",") # agora separa por ","
 
 print(100 * "=")
-
 print(f"frase original: {frase}\npalavras separadas: {lista_palavras}")
 print(f"frases separadaS: {lista_frases}\nRemovendo os espaços: ")
+print(100 * "=")
 
 for i, _ in enumerate(lista_frases):
     print(lista_frases[i].strip()) # strip = remove os espaços no começo e final
 
+print(100 * "=")
+print(f"frases reunidas por join: {'-'.join(lista_frases)}") # une as strings de uma lista 
 print(100 * "=")
