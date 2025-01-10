9ea77613a86e47df4f30345408ac12698ee071e1
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-10 15:29:55 -0300

    aula065

---
+++ secao_04[funcoes_dicionarios_modulos_etc]/aulas/aula065[def].py
@@ -0,0 +1,11 @@
+def linha_top():
+    print(100 * "=")
+
+def saudacao(nome="Seu nome"):
+    print(f"seje bem vindo(a), {nome}!")
+
+linha_top()
+saudacao("Eduardo")
+saudacao(nome="Tatiana")
+saudacao()
+linha_top()
