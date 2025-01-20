06f89cc5c43d68fa7a2f0fd59de4d09d5765bc50
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-20 11:16:52 -0300

    aula078_1

---
+++ secao_04[funcoes_dicionarios_modulos_etc]/aulas/aula078_1[set_and_methods].py
@@ -0,0 +1,23 @@
+# set = {}, set() | elimina itens duplicados | não garante a ordem
+nums = [1, 2, 3, 3, 3, 3, 4, 4, 4]
+set_1 = set(nums) # não recebe mutaveis [], {} e etc
+
+print(100 * "=")
+print(f"lista normal: {nums}\ncom set: {set_1}")
+print(100 * "=")
+
+set_1.add(5) # só aceita um valor
+set_1.update(("Olá mundão", 5, 6, 7)) # () para adicionar mais valores e strings
+
+print(f"após add e update: {set_1}")
+print(100 * "=")
+
+set_1.discard("Olá mundão") # não recebe index
+
+print(f"após discard: {set_1}")
+print(100 * "=")
+
+set_1.clear()
+
+print(f"após clear: {set_1}")
+print(100 * "=")
