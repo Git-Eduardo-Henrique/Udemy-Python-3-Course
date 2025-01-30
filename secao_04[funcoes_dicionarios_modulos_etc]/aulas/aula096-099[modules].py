b270d0e2ed7187715182ab276138129214b74ee3
Eduardo Henrique <eduardos8236@gmail.com>
2025-01-30 12:04:32 -0300

    aula099.py

--- secao_04[funcoes_dicionarios_modulos_etc]/aulas/aula096-099[modules].py
+++ secao_04[funcoes_dicionarios_modulos_etc]/aulas/aula096-099[modules].py
@@ -1,10 +1,10 @@
-import aula097_m
-from aula097_m import somar
+from aula096_099_package import module
+from aula096_099_package.module import somar
 
 print("Este é o modulo: ", __name__)
 print(100 * "=")
 
 print("Usando função do modulo:")
-print(aula097_m.somar(2, 2))
+print(module.somar(2, 2))
 print(somar(21, 21))
 print(100 * "=")
